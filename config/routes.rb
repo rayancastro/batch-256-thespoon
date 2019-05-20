7bad0457f442c49cc6d54d75cdeace78541fbe13
Rayan Castro <rayancdc@gmail.com>
2019-05-20 11:57:15 -0300

    Lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+
+  # VERB PATH , to: 'CONTROLLER#ACTION'
+
+  root to: 'pages#home'
+
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+
+  # RESTAURANTS
+
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  get '/restaurants', to: 'restaurants#index'
+
+  post '/restaurants', to: 'restaurants#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
